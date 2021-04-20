fb3926ad9195a2bc88ce2d57588410633cd3b3eb
xiangwei <18570355743@163.com>
2021-04-20 15:38:48 +0800

    first list: seq and link

---
+++ cmake-build-debug/CMakeFiles/mydemo.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/xiangwei/CLionProjects/mydemo/list/link/LinkListDemo.cpp" "/Users/xiangwei/CLionProjects/mydemo/cmake-build-debug/CMakeFiles/mydemo.dir/list/link/LinkListDemo.cpp.o"
+  "/Users/xiangwei/CLionProjects/mydemo/list/seq/SeqListDemo.cpp" "/Users/xiangwei/CLionProjects/mydemo/cmake-build-debug/CMakeFiles/mydemo.dir/list/seq/SeqListDemo.cpp.o"
+  "/Users/xiangwei/CLionProjects/mydemo/main.cpp" "/Users/xiangwei/CLionProjects/mydemo/cmake-build-debug/CMakeFiles/mydemo.dir/main.cpp.o"
+  "/Users/xiangwei/CLionProjects/mydemo/universal/Universal.cpp" "/Users/xiangwei/CLionProjects/mydemo/cmake-build-debug/CMakeFiles/mydemo.dir/universal/Universal.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
